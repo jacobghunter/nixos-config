aab1e1f174b7562d088a05f1061fe09af5848019
Jacob Hunter <jacobguinhunter@gmail.com>
2026-02-23 03:25:30 +0000

    add instructions for setting up serial in wsl

--- wsl/configuration.nix
+++ wsl/configuration.nix
@@ -9,6 +9,11 @@
   wsl.enable = true;
   wsl.defaultUser = "jacob";
   
+  # To set up serial port on wsl:
+  # winget install usbipd
+  # usbipd list
+  # usbipd bind --busid <id>
+  # usbipd attach --wsl --busid <id>
   system.activationScripts.modprobeSymlink = ''
    # The -f flag ensures this command works even if the link already exists
    ln -sf ${pkgs.kmod}/bin/modprobe /usr/bin/modprobe
