1fceab6f30002559420199c4bd5df2d0bb22c5f1
Jacob Hunter <jacobguinhunter@gmail.com>
2026-02-27 23:39:21 -0700

    fix monitor config for pc

--- pc/modules/hyprland/home.nix
+++ pc/modules/hyprland/home.nix
@@ -6,7 +6,7 @@
   ];
 
   wayland.windowManager.hyprland.extraConfig = lib.mkAfter ''
-    # PC monitor configuration
+    # PC monitor configuration (simplified for debugging)
     monitor=HDMI-A-1,2560x1440@59.97300,0x0,1
     monitor=DP-1,3440x1440@174.96,2560x0,1,bitdepth,10,cm,hdr,sdrbrightness,1.2,sdrsaturation,1.05,vrr,1
   '';
