84ab7c007430e3a59ce4ce3518dbca381fc0f94c
Jacob Hunter <jacobguinhunter@gmail.com>
2026-06-10 20:05:29 -0700

    fix: kitty auto reload config issue

--- nixos-shared/modules/kitty/kitty.nix
+++ nixos-shared/modules/kitty/kitty.nix
@@ -55,6 +55,7 @@ in
         size = 13;
       };
       settings = {
+        auto_reload_config = "no";
         window_padding_width = 8;
         italic_font = "auto";
         bold_italic_font = "auto";
