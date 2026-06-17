7ab834390c3ff5ab4ffa5ff88d25c12214b8dcea
Jacob Hunter <jacobguinhunter@gmail.com>
2026-06-16 22:16:16 -0700

    fix: disable audio devices on PC

--- nixos-pc/home.nix
+++ nixos-pc/home.nix
@@ -15,10 +15,11 @@
 
   xdg.configFile."wireplumber/wireplumber.conf.d/52-disable-devices.conf".text = ''
     monitor.alsa.rules = [
+      # 1. Disable Samson Q2U headphone/output node (leaves mic input active)
       {
         matches = [
           {
-            node.name = "alsa_output.pci-0000_00_1f.3.hdmi-stereo"
+            node.name = "alsa_output.usb-Samson_Technologies_Samson_Q2U_Microphone-00.analog-stereo"
           }
         ]
         actions = {
@@ -27,10 +28,37 @@
           }
         }
       }
+      # 2. Disable motherboard built-in audio controller (Ryzen HD Audio)
       {
         matches = [
           {
-            device.name = "alsa_card.pci-0000_08_00.1"
+            device.name = "alsa_card.pci-0000_10_00.6"
+          }
+        ]
+        actions = {
+          update-props = {
+            device.disabled = true
+          }
+        }
+      }
+      # 3. Disable GPU Navi 31 HDMI/DP Audio Controller
+      {
+        matches = [
+          {
+            device.name = "alsa_card.pci-0000_03_00.1"
+          }
+        ]
+        actions = {
+          update-props = {
+            device.disabled = true
+          }
+        }
+      }
+      # 4. Disable GPU Radeon High Definition Audio Controller
+      {
+        matches = [
+          {
+            device.name = "alsa_card.pci-0000_10_00.1"
           }
         ]
         actions = {
