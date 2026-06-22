c5c35c8f158921bede27380495581a907d986eb6
Jacob Hunter <jacobguinhunter@gmail.com>
2026-06-21 20:21:08 -0700

    fix: hypridle laptop config

--- nixos-laptop/modules/hyprland/home.nix
+++ nixos-laptop/modules/hyprland/home.nix
@@ -8,8 +8,6 @@
   modules.hyprland.idleTimeout = 150; # 2.5 minutes
   modules.hyprland.dpmsTimeout = 300; # 5 minutes
 
-  services.hypridle.settings.general.after_sleep_cmd = "hyprctl dispatch dpms on && brightnessctl -r && (sleep 8 && systemctl --user restart wayle.service &)";
-
   wayland.windowManager.hyprland.extraConfig = lib.mkAfter ''
     monitor=,preferred,auto,1,mirror,DP-1
   '';
