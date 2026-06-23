0f332977b9f56745148931c4d4f2381082f5b522
Jacob Hunter <jacobguinhunter@gmail.com>
2026-06-22 20:38:49 -0700

    feat: vimuim c for firefox

--- nixos-shared/graphical/modules/firefox/firefox.nix
+++ nixos-shared/graphical/modules/firefox/firefox.nix
@@ -27,6 +27,10 @@
           install_url = "https://addons.mozilla.org/firefox/downloads/latest/bitwarden-password-manager/latest.xpi";
           installation_mode = "force_installed";
         };
+        "vimium-c@gdh1995.cn" = {
+          install_url = "https://addons.mozilla.org/firefox/downloads/latest/vimium-c/latest.xpi";
+          installation_mode = "force_installed";
+        };
       };
     };
 
