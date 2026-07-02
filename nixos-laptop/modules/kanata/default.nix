35e5487095757ba0d878925e24a23381c7ff0b60
Jacob Hunter <jacobguinhunter@gmail.com>
2026-07-01 20:51:21 -0700

    fix: kanata binds

--- nixos-laptop/modules/kanata/default.nix
+++ nixos-laptop/modules/kanata/default.nix
@@ -1,32 +1,31 @@
 { config, pkgs, ... }:
 
 let
-  customKanata = pkgs.kanata.overrideAttrs (oldAttrs: rec {
-    version = "1.12.0-prerelease-2";
-    src = pkgs.fetchFromGitHub {
-      owner = "jtroo";
-      repo = "kanata";
-      rev = "v${version}";
-      hash = "sha256-bNUlQBsyGxCu3GHP+qgrYLikLagXxzLjjuZFZFi7Vzk=";
+  version = "1.12.0-prerelease-2";
+
+  src = pkgs.fetchFromGitHub {
+    owner = "jtroo";
+    repo = "kanata";
+    rev = "v${version}";
+    hash = "sha256-bNUlQBsyGxCu3GHP+qgrYLikLagXxzLjjuZFZFi7Vzk=";
+  };
+
+  customKanata = pkgs.kanata.overrideAttrs (oldAttrs: {
+    inherit version src;
+    doCheck = false;
+    cargoDeps = pkgs.rustPlatform.fetchCargoVendor {
+      inherit src;
+      name = "kanata-vendor-${version}.tar.gz";
+      hash = "sha256-da7kmSvm+z6C+RPqEBEY9PNWxrAEQ8h/ZGDvS9WJ1J8=";
     };
-    cargoDeps = oldAttrs.cargoDeps.overrideAttrs (
-      pkgs.lib.const {
-        name = "kanata-vendor.tar.gz";
-        inherit src;
-        # Updated to the hash Nix is expecting ("got") from your build logs
-        outputHash = "sha256-da7kmSvm+z6C+RPqEBEY9PNWxrAEQ8h/ZGDvS9WJ1J8=";
-      }
-    );
   });
 in
 {
   hardware.uinput.enable = true;
-
   boot.kernelModules = [ "uinput" ];
 
   services.kanata = {
     enable = true;
-    # Explicitly hook up the newly scoped custom package
     package = customKanata;
 
     keyboards = {
@@ -36,6 +35,15 @@ in
         ];
         configFile = ./kanata.kbd;
       };
+    };
+  };
+
+  # Explicitly append the necessary hardware groups to Kanata's systemd service
+  systemd.services.kanata-internal = {
+    serviceConfig = {
+      User = "root";
+      Group = "root";
+      DynamicUser = pkgs.lib.mkForce false; # Disable the restrictive dynamic user isolation
     };
   };
 }
